8d5a5c043137b3737b9bbefae355deb04eef4683
Cheng Zhang <3ki5tj@gmail.com>
2014-11-11 17:54:05 -0600

    add configuration files

--- web/js/cfg.js
+++ web/js/cfg.js
@@ -32,6 +32,13 @@ function change_unit_eps()
   $("#unit_eps12").val( $("#unit_eps6").val() );
 }
 
+function change_eps6(id)
+{
+  if ( $("#sameeps").val() == "on" ) {
+    $(("#eps12_" + id)).val( $(("#eps6_" + id)).val() );
+  }
+}
+
 $(document).ready(function() {
   $("#ns").val("0");
   change_ns();
@@ -48,12 +55,20 @@ function gencfg()
   }
   s += "ns            = " + ns + "\n";
 
+  var ljtype = $("#ljtype").val();
+  var sameeps = $("#sameeps").val();
   for ( var i = 0; i < ns; i++ ) {
     var i1 = i + 1;
     s += "\n";
     s += "sigma(" + i1 + ")      = " + $(("#sigma_"  + i1)).val() + "\n";
-    s += "eps6(" + i1 + ")       = " + $(("#eps6_"   + i1)).val() + "\n";
-    s += "eps12(" + i1 + ")      = " + $(("#eps12_"  + i1)).val() + "\n";
+    if ( ljtype != "Hard-sphere" ) {
+      if ( sameeps == "on") {
+        s += "eps(" + i1 + ")        = " + $(("#eps6_"   + i1)).val() + "\n";
+      } else {
+        s += "eps6(" + i1 + ")       = " + $(("#eps6_"   + i1)).val() + "\n";
+        s += "eps12(" + i1 + ")      = " + $(("#eps12_"  + i1)).val() + "\n";
+      }
+    }
     s += "rho(" + i1 + ")        = " + $(("#rho_"    + i1)).val() + "\n";
     s += "charge(" + i1 + ")     = " + $(("#charge_" + i1)).val() + "\n";
   }
@@ -63,15 +78,23 @@ function gencfg()
   cb = $("#chembonds").val().split("\n");
   for ( var i = 0; i < cb.length; i++ ) {
     var ln = cb[i].trim();
-    if ( ln.charAt(0) == "#" ) continue;
+    if ( ln.charAt(0) == "#" || ln.length == 0 ) continue;
     var arr = ln.split(" ");
+    if ( arr.length < 3 ) {
+      console.log("invalid line " + ln);
+      continue;
+    }
     s += "dis(" + arr[0] + ", " + arr[1] + ")     = " + arr[2] + "\n";
   }
   s += "\n";
 
   var unit_eps = $("#unit_eps6").val();
   var kBT, kBU, ampch;
-  if ( unit_eps == "K" ) {
+  if ( unit_eps == "reduced" ) {
+    kBT = 1;
+    kBU = 1;
+    ampch = 1;
+  } else if ( unit_eps == "K" ) {
     kBT = 1;
     kBU = "KBNA";
     ampch = "KE2PK";
@@ -95,6 +118,7 @@ function gencfg()
   s += "rmax          = " + $("#rmax").val() + "\n";
   s += "npt           = " + $("#npt").val() + "\n";
   s += "nlambdas      = " + $("#nlambdas").val() + "\n";
+  s += "itmax         = " + $("#itmax").val() + "\n";
   s += "tol           = " + $("#tol").val() + "\n";
   s += "solver        = " + $("#solver").val() + "\n";
   s += "picard_damp   = " + $("#picard_damp").val() + "\n";
